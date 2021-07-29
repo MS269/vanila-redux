a0a2ef9804b3f6edc56b7edb18451a2ed6d7abfe
MS269 <ehddnr9797@gmail.com>
2021-07-30 00:09:29 +0900

    react-redux: todo

---
+++ src/routes/Home.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+
+import { actionCreators } from "../store";
+
+import ToDo from "../components/ToDo";
+
+const Home = ({ toDos, addToDo }) => {
+  const [text, setText] = useState("");
+  const onChange = (event) => setText(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addToDo(text);
+    setText("");
+  };
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => ({ toDos: state });
+
+const mapDispatchToProps = (dispatch) => ({
+  addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ src/store.js
@@ -0,0 +1,25 @@
+import { createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+const addToDo = (text) => ({ type: ADD, text });
+
+const deleteToDo = (id) => ({ type: DELETE, id: parseInt(id) });
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ id: Date.now(), text: action.text }, ...state];
+    case DELETE:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export const actionCreators = { addToDo, deleteToDo };
+
+export default store;
